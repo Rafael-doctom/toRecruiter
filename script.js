a76d3459581454878c912d2e5400c66026d818f5
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-01-06 19:18:30 -0300

    review project

--- script.js
+++ script.js
@@ -3,50 +3,51 @@ var dataMap = [
         Technologies: 'HTML, CSS e JAVASCRIPT',
         Nameprojects: 'Agência de Viagens',
         linkOfProject: 'https://rafael-doctom.github.io/agencia-de-viagens/',
-        Namecourses: 'Crud',
-        linkOfCourse: 'https://rafael-doctom.github.io/crud-js/',
+        linkOfRepo: 'https://github.com/Rafael-doctom/agencia-de-viagens',
+        NameRepo: 'agencia-de-viagens',
     },
     {
         Technologies: 'HTML, CSS e JAVASCRIPT',
         Nameprojects: 'E-commerce product page ',
         linkOfProject: 'https://rafael-doctom.github.io/E-commerce-product-page-challenge-hub/',
-        Namecourses: '',
-        linkOfCourse: '',
+        linkOfRepo: 'https://github.com/Rafael-doctom/E-commerce-product-page-challenge-hub',
+        NameRepo: 'E-commerce-product-page-challenge-hub',
+
     },
     {
         Technologies: 'HTML, CSS',
         Nameprojects: 'FlexBlog',
         linkOfProject: 'https://rafael-doctom.github.io/FlexBlog/',
-        Namecourses: '',
-        linkOfCourse: '',
+        linkOfRepo: 'https://github.com/Rafael-doctom/FlexBlog',
+        NameRepo: 'FlexBlog',
     },
     {
         Technologies: 'React.js',
         Nameprojects: 'Search Cep API',
         linkOfProject: 'https://search-cep-api.netlify.app/',
-        Namecourses: '',
-        linkOfCourse: '',
+        linkOfRepo: 'https://github.com/Rafael-doctom/search-cep-api',
+        NameRepo: 'search-cep-api',
     },
     {
         Technologies: 'HTML e JAVASCRIPT',
         Nameprojects: 'Chart.js ',
         linkOfProject: 'https://rafael-doctom.github.io/chart.js/',
-        Namecourses: '',
-        linkOfCourse: '',
+        linkOfRepo: 'https://github.com/Rafael-doctom/chart.js',
+        NameRepo: 'chart.js',
     },
     {
         Technologies: 'HTML e CSS',
         Nameprojects: 'Caravan',
         linkOfProject: 'https://rafael-doctom.github.io/caravan/',
-        Namecourses: '',
-        linkOfCourse: '',
+        linkOfRepo: 'https://github.com/Rafael-doctom/caravan',
+        NameRepo: 'caravan',
     },
     {
         Technologies: 'React.js, Hooks & Tailwind.css',
         Nameprojects: 'Login',
         linkOfProject: 'https://modalsrct.netlify.app/',
-        Namecourses: '',
-        linkOfCourse: '',
+        linkOfRepo: 'https://github.com/Rafael-doctom/modals',
+        NameRepo: 'login',
     },
 
 ];
@@ -63,8 +64,8 @@ var describeMap = dataMap.map(function (item, indice) {
                     </a>                
                 </td>
                 <td>
-                    <a href="${item.linkOfCourse}" target="_blank">
-                        ${item.Namecourses}
+                    <a href="${item.linkOfRepo}" target="_blank">
+                        ${item.NameRepo}
                     </a>                
                 </td>
             </tr>`;
