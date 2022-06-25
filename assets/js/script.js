e96ed8fa145e763826dc5431cf989afb56bc7729
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-06-25 15:59:19 -0300

    feat: data included

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,114 @@
 var dataMap = [
     {
         Technologies: 'Html, Css e Js',
+        Nameprojects: 'conecta-life',
+        review: 'https://rafael-doctom.github.io/conecta-life/',
+        repository: 'https://github.com/Rafael-doctom/conecta-life',
+        NameRepo: 'conecta-life',
+        describe: 'em desenvolvimento...',
+    },
+    {
+        Technologies: 'Html, Css e Js',
+        Nameprojects: 'date-and-time',
+        review: 'https://rafael-doctom.github.io/date-and-time/',
+        repository: 'https://github.com/Rafael-doctom/date-and-time',
+        NameRepo: 'date-and-time',
+        describe: 'em desenvolvimento...',
+    },
+    {
+        Technologies: 'Html, Css e Js',
+        Nameprojects: 'fone-landing',
+        review: 'https://rafael-doctom.github.io/fone-landing/',
+        repository: 'https://github.com/Rafael-doctom/fone-landing',
+        NameRepo: 'fone-landing',
+        describe: 'em desenvolvimento...',
+    },
+    {
+        Technologies: 'Html, Css',
+        Nameprojects: 'RocketCoffee',
+        review: 'https://rafael-doctom.github.io/RocketCoffee/',
+        repository: 'https://github.com/Rafael-doctom/RocketCoffee',
+        NameRepo: 'RocketCoffee',
+        describe: 'em desenvolvimento...',
+    },
+    {
+        Technologies: 'Html, Css e Js',
         Nameprojects: 'typewriter-effect',
-        linkOfProject: 'https://rafael-doctom.github.io/typewriter-effect/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/typewriter-effect',
+        review: 'https://rafael-doctom.github.io/typewriter-effect/',
+        repository: 'https://github.com/Rafael-doctom/typewriter-effect',
         NameRepo: 'typewriter-effect',
         describe: 'Efeito de texto sendo renderizado como uma máquina de escrever.',
     },
     {
         Technologies: 'React Native & UseState',
         Nameprojects: 'imc',
-        linkOfProject: 'https://github.com/Rafael-doctom/imc',
-        linkOfRepo: 'https://github.com/Rafael-doctom/imc',
+        review: 'https://github.com/Rafael-doctom/imc',
+        repository: 'https://github.com/Rafael-doctom/imc',
         NameRepo: 'imc',
         describe: 'Calculadora de IMC. O Imc é reconhecido como padrão internacional para avaliar o grau de sobrepeso e obesidade.',
     },
     {
         Technologies: 'Html, Css e Js',
         Nameprojects: 'rocketFlix_',
-        linkOfProject: 'https://rafael-doctom.github.io/rocketFlix_/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/rocketFlix_',
+        review: 'https://rafael-doctom.github.io/rocketFlix_/',
+        repository: 'https://github.com/Rafael-doctom/rocketFlix_',
         NameRepo: 'rocketFlix_',
         describe: 'Desafio para o discover da rocketseat | Challenge by Rocketseat.',
     },
     {
         Technologies: 'Html, Css e Js',
         Nameprojects: 'nlw_heat_challenge-01',
-        linkOfProject: 'https://rafael-doctom.github.io/nlw_heat_challenge-01/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/nlw_heat_challenge-01',
+        review: 'https://rafael-doctom.github.io/nlw_heat_challenge-01/',
+        repository: 'https://github.com/Rafael-doctom/nlw_heat_challenge-01',
         NameRepo: 'nlw_heat_challenge-01',
         describe: 'Crachá virtual criado na trilha Origin do NLW/Heat da Rocketseat.',
     },
-    {
-        Technologies: 'Html, Css e Js',
-        Nameprojects: 'text_generator',
-        linkOfProject: 'https://rafael-doctom.github.io/text_generator/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/text_generator',
-        NameRepo: 'text_generator',
-        describe: 'Trata-se de um gerador de texto, que consome dados de uma API, ela que gera os dados de maneira aleatória.',
-    },
+
     {
         Technologies: 'React.js & UseState',
         Nameprojects: 'teamSoft_frontEnd',
-        linkOfProject: 'https://team-soft.netlify.app/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/teamSoft_frontEnd',
+        review: 'https://team-soft.netlify.app/',
+        repository: 'https://github.com/Rafael-doctom/teamSoft_frontEnd',
         NameRepo: 'teamSoft_frontEnd',
         describe: 'Trata-se de um teste técnico, que é desevolver uma página de Dellivery, possui algumas funcionalides, como: Buscar produto, + de 1 produto disponível, Tela adaptada p/ dispositivos móveis, Adicionar Talher + PopUp de confirmação, Consumo de API.'
     },
     {
         Technologies: 'React.js & UseState',
         Nameprojects: 'challenge01_codelandia',
-        linkOfProject: 'https://challenge01-codelandia.netlify.app/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/challenge01_codelandia',
+        review: 'https://challenge01-codelandia.netlify.app/',
+        repository: 'https://github.com/Rafael-doctom/challenge01_codelandia',
         NameRepo: 'challenge01_codelandia',
         describe: 'Este é um Desafio de recriar o Layout através do Figma. - Trata-se de uma página de blog desenhada por Iuri Silva(Codelândia). Possui algumas funcionalides, como: Pesquisar, DarkMode e Consumo de API.'
     },
     {
         Technologies: 'Html, Bootstrap e Js',
         Nameprojects: 'login-localStorage',
-        linkOfProject: 'https://rafael-doctom.github.io/login-localStorage/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/login-localStorage',
+        review: 'https://rafael-doctom.github.io/login-localStorage/',
+        repository: 'https://github.com/Rafael-doctom/login-localStorage',
         NameRepo: 'login-localStorage',
         describe: 'Página de Login, possui funcionalidades, como: Autenticação de Login, armazenar dados no localStorage.'
     },
     {
         Technologies: 'Python3',
         Nameprojects: 'Real quote',
-        linkOfProject: 'https://github.com/Rafael-doctom/real-quote',
-        linkOfRepo: 'https://github.com/Rafael-doctom/real-quote',
+        review: 'https://github.com/Rafael-doctom/real-quote',
+        repository: 'https://github.com/Rafael-doctom/real-quote',
         NameRepo: 'real-quote',
         describe: 'Aplicação feita em Python, utilizando o Tkinter para criar a interface gráfica, serve para buscar informações sobre cotação em tempo real.'
     },
     {
         Technologies: 'Next.JS',
         Nameprojects: 'api-json-placeholder',
-        linkOfProject: 'https://api-json-placeholder.netlify.app/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/api-json-placeholder',
+        review: 'https://api-json-placeholder.netlify.app/',
+        repository: 'https://github.com/Rafael-doctom/api-json-placeholder',
         NameRepo: 'api-json-placeholder',
         describe: 'Aplicação em Next.Js que consume API'
     },
     {
         Technologies: 'Html, Css e Bootstrap',
         Nameprojects: 'Finans',
-        linkOfProject: 'https://rafael-doctom.github.io/finans/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/finans',
+        review: 'https://rafael-doctom.github.io/finans/',
+        repository: 'https://github.com/Rafael-doctom/finans',
         NameRepo: 'finans',
         describe: 'Desenvolvida durante o curso da Origamid'
     },
@@ -91,98 +116,98 @@ var dataMap = [
     {
         Technologies: 'Html, Css e Js',
         Nameprojects: 'E-commerce product page ',
-        linkOfProject: 'https://rafael-doctom.github.io/E-commerce-product-page-challenge-hub/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/E-commerce-product-page-challenge-hub',
+        review: 'https://rafael-doctom.github.io/E-commerce-product-page-challenge-hub/',
+        repository: 'https://github.com/Rafael-doctom/E-commerce-product-page-challenge-hub',
         NameRepo: 'E-commerce-product-page-challenge-hub',
         describe: 'Desafio FrontEnd Mentor'
     },
     {
         Technologies: 'Html e Css',
         Nameprojects: 'FlexBlog',
-        linkOfProject: 'https://rafael-doctom.github.io/FlexBlog/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/FlexBlog',
+        review: 'https://rafael-doctom.github.io/FlexBlog/',
+        repository: 'https://github.com/Rafael-doctom/FlexBlog',
         NameRepo: 'FlexBlog',
         describe: 'Desenvolvida durante o curso da Origamid'
     },
     {
         Technologies: 'React.js',
         Nameprojects: 'Search Cep API',
-        linkOfProject: 'https://search-cep-api.netlify.app/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/search-cep-api',
+        review: 'https://search-cep-api.netlify.app/',
+        repository: 'https://github.com/Rafael-doctom/search-cep-api',
         NameRepo: 'search-cep-api',
         describe: 'Aplicação que informa o endereço com base no CEP fornecido'
     },
     {
         Technologies: 'Html e JavaScript',
         Nameprojects: 'Chart.js ',
-        linkOfProject: 'https://rafael-doctom.github.io/chart.js/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/chart.js',
+        review: 'https://rafael-doctom.github.io/chart.js/',
+        repository: 'https://github.com/Rafael-doctom/chart.js',
         NameRepo: 'chart.js',
-        describe: '...'
+        describe: 'em desenvolvimento...',
     },
     {
         Technologies: 'Html, Css, Bootstrap e JavaScript',
         Nameprojects: 'set-cookies',
-        linkOfProject: 'https://rafael-doctom.github.io/set-cookies/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/set-cookies',
+        review: 'https://rafael-doctom.github.io/set-cookies/',
+        repository: 'https://github.com/Rafael-doctom/set-cookies',
         NameRepo: 'set-cookies',
-        describe: ''
+        describe: 'em desenvolvimento...',
     },
     {
         Technologies: 'Html e Css',
         Nameprojects: 'Caravan',
-        linkOfProject: 'https://rafael-doctom.github.io/caravan/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/caravan',
+        review: 'https://rafael-doctom.github.io/caravan/',
+        repository: 'https://github.com/Rafael-doctom/caravan',
         NameRepo: 'caravan',
-        describe: ''
+        describe: 'em desenvolvimento...',
     },
     {
         Technologies: 'React.js, Hooks & Tailwind.css',
         Nameprojects: 'Login',
-        linkOfProject: 'https://modalsrct.netlify.app/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/modals',
+        review: 'https://modalsrct.netlify.app/',
+        repository: 'https://github.com/Rafael-doctom/modals',
         NameRepo: 'login',
-        describe: ''
+        describe: 'em desenvolvimento...',
     },
     {
         Technologies: 'Html, Css e Js',
         Nameprojects: 'acender-lampada',
-        linkOfProject: 'https://rafael-doctom.github.io/acender-lampada/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/acender-lampada',
+        review: 'https://rafael-doctom.github.io/acender-lampada/',
+        repository: 'https://github.com/Rafael-doctom/acender-lampada',
         NameRepo: 'acender-lampada',
-        describe: ''
+        describe: 'em desenvolvimento...',
     },
     {
         Technologies: 'Python3',
         Nameprojects: 'api-cotação',
-        linkOfProject: 'https://github.com/Rafael-doctom/apiPython',
-        linkOfRepo: 'https://github.com/Rafael-doctom/apiPython',
+        review: 'https://github.com/Rafael-doctom/apiPython',
+        repository: 'https://github.com/Rafael-doctom/apiPython',
         NameRepo: 'api-cotação',
-        describe: ''
+        describe: 'em desenvolvimento...',
     },
     {
         Technologies: 'Html, Css e Js',
         Nameprojects: 'sistema-de-multas',
-        linkOfProject: 'https://rafael-doctom.github.io/sistema-de-multas/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/sistema-de-multas',
+        review: 'https://rafael-doctom.github.io/sistema-de-multas/',
+        repository: 'https://github.com/Rafael-doctom/sistema-de-multas',
         NameRepo: 'sistema-de-multas',
-        describe: ''
+        describe: 'em desenvolvimento...',
     },
     {
         Technologies: 'Html, Css e Js',
         Nameprojects: 'contador',
-        linkOfProject: 'https://rafael-doctom.github.io/contador/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/contador',
+        review: 'https://rafael-doctom.github.io/contador/',
+        repository: 'https://github.com/Rafael-doctom/contador',
         NameRepo: 'contador',
-        describe: ''
+        describe: 'em desenvolvimento...',
     },
     {
         Technologies: 'Html, Css e Js',
         Nameprojects: 'Agência de Viagens',
-        linkOfProject: 'https://rafael-doctom.github.io/agencia-de-viagens/',
-        linkOfRepo: 'https://github.com/Rafael-doctom/agencia-de-viagens',
+        review: 'https://rafael-doctom.github.io/agencia-de-viagens/',
+        repository: 'https://github.com/Rafael-doctom/agencia-de-viagens',
         NameRepo: 'agencia-de-viagens',
-        describe: ''
+        describe: 'em desenvolvimento...',
     }
 ];
 
@@ -193,12 +218,12 @@ var describeMap = dataMap.map(function (item) {
                     ${item.Technologies}
                 </td>
                 <td  class="describe2">
-                    <a href="${item.linkOfProject}" target="_blank">
+                    <a href="${item.review}" target="_blank">
                         ${item.Nameprojects}
                     </a>                
                 </td>
                 <td  class="describe3">
-                    <a href="${item.linkOfRepo}" target="_blank">
+                    <a href="${item.repository}" target="_blank">
                         ${item.NameRepo}
                     </a>                
                 </td>
