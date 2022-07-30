40471e67352c28107f83c18536c7d60c77b951ab
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-07-30 13:57:23 -0300

    feat: input & table added with included tailwind

---
+++ torecruiter/src/components/table/index.js
@@ -0,0 +1,69 @@
+const Table = () => {
+
+    return (
+        <>
+            <table className="text-left table mt-1 table-hover w-full">
+                <thead className=" border-t-2 ">
+                    <tr className="">
+                        <th class="py-2 pl-1">Tecnologias</th>
+                        <th class="py-2 pl-1">Ver Projeto</th>
+                        <th class="py-2 pl-1">Repositório</th>
+                        <th class="py-2 pl-1">Descrição</th>
+                    </tr>
+                </thead>
+                <tbody className="">
+                    <tr className="hover:bg-gray-100  border-y-2 ">
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                    </tr>
+                    <tr className="hover:bg-gray-100  border-y-2">
+                        <td class="">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                    </tr>
+                    <tr className="hover:bg-gray-100  border-y-2">
+                        <td class="">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                    </tr>
+                    <tr className="hover:bg-gray-100  border-y-2">
+                        <td class="">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                    </tr>
+                    <tr className="hover:bg-gray-100  border-y-2">
+                        <td class="">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                    </tr>
+                    <tr className="hover:bg-gray-100  border-y-2">
+                        <td class="">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                    </tr>
+                    <tr className="hover:bg-gray-100  border-y-2">
+                        <td class="">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                    </tr>
+                    <tr className="hover:bg-gray-100  border-y-2">
+                        <td class="">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                        <td class="py-3  pl-1">Indiana</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
+};
+
+export default Table;
--- torecruiter/src/pages/home/index.js
+++ torecruiter/src/pages/home/index.js
@@ -1,11 +1,16 @@
+import Input from "../../components/input";
+import Table from "../../components/table";
 
+export default function Home() {
+  return (
+    <>
+      <div className="relative">
+        <div className="max-w-8xl mx-auto mx-5 mt-3">
+          <Input />
+          <Table />
+        </div>
+      </div>
+    </>
+  )
+};
 
-const Home = () => {
-    return(
-        <>
-        <button className="bg-red-600">Button</button>
-        </>
-    )
-}
-
-export default Home;
