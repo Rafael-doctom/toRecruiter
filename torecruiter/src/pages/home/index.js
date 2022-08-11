6e82362868a5f76efbb40b0c5a7f282344607a37
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-08-11 17:11:57 -0300

    feat: modal included e basic style

--- torecruiter/src/pages/home/index.js
+++ torecruiter/src/pages/home/index.js
@@ -3,12 +3,32 @@ import ScrollTop from "../../components/scrollTop";
 import Table from "../../components/table";
 
 export default function Home() {
+
+  const removeModal = () => {
+
+    let modal = document.getElementById("modal");
+
+    if (modal.style.display === 'block') {
+      modal.style.display = 'none'
+    } {
+      modal.style.display = 'none'
+    };
+  };
+
   return (
     <>
       <div className="relative">
         <div className="max-w-8xl mx-auto  mx-5 mt-4 " >
           <Input />
           <Table />
+        </div>
+      </div>
+      <div id="modal">
+        <div class="teste2">
+          <button type="button" class="close" onClick={removeModal}>
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <span>Projetos mais recentes estão no topo! 🤯 </span>
         </div>
       </div>
       <ScrollTop />
