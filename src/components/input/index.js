6775a552e86d15b77fc9d75d3d210d1e5cd37aa9
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-09-05 14:55:39 -0300

    state for input add

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Input = () => {
 
     let inputStyle = {
@@ -10,6 +12,9 @@ const Input = () => {
         border: 0,
     };
 
+    const [valueInput, setInput] = useState(false);
+
+
     return (
         <>
             <div className="mt-4 py-1 border-b-2 bg-gray-000">
@@ -20,14 +25,18 @@ const Input = () => {
                         className="outline-0 h-10 block w-full pl-1 rounded-md"
                         placeholder="Digite algo..."
                         style={inputStyle}
+                        onClick={() => setInput('Em desenvolvimento...')}
                     />
+
                     <div className="absolute inset-y-0 right-0 flex items-center">
                         <button className=" h-10 w-40" style={bgSearch}>
                             Pesquisar
                         </button>
                     </div>
                 </div>
-
+                <p className="" style={{color:'rgb(0, 123, 255)', fontWeight:500, fontFamily:'monospace', paddingTop:'0.2rem'}}>
+                    {valueInput}
+                </p>
             </div>
         </>
     );
